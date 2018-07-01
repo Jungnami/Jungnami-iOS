bff2aad7e3623cd56473922f31702e70c931aea7
강수진 <rkdthd1234@naver.com>
2018-07-02 07:25:41 +0900

    *뷰 구성/키보드 대응 1. CommunityWriteView 오토레이아웃 구성 2. 내용에 따라 텍스트 뷰 크기 달라도록 3. 키보드 놓는 위치에 따라 텍스트뷰 보이는 위치 달라지도록 키보드 대응

--- Jungnami/Jungnami/VC/CommunityWrite/CommunityWriteVC.swift
+++ Jungnami/Jungnami/VC/CommunityWrite/CommunityWriteVC.swift
@@ -8,28 +8,82 @@
 
 import UIKit
 
-class CommunityWriteVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class CommunityWriteVC: UIViewController, UITextViewDelegate {
+    
+    
+    @IBAction func dismissBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var profileImgView: UIImageView!
+    @IBOutlet weak var contentTxtView: UITextView!
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    var keyboardDismissGesture: UITapGestureRecognizer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setKeyboardSetting()
+        profileImgView.makeImageRound()
+        self.contentTxtView.delegate = self
     }
-    */
+    
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView.text.count > 0 {
+            print(textView.text)
+            doneBtn.setImage(#imageLiteral(resourceName: "writepage_complete_blue_button"), for: .normal)
+            doneBtn.isUserInteractionEnabled = true
+        } else {
+             doneBtn.setImage(#imageLiteral(resourceName: "writepage_complete_gray_button"), for: .normal)
+            doneBtn.isUserInteractionEnabled = false
+        }
+    }
 
 }
+
+//키보드 대응
+extension CommunityWriteVC {
+    func setKeyboardSetting() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        adjustKeyboardDismissGesture(isKeyboardVisible: true)
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+           
+            let keyboardEndframe = self.view.convert(keyboardSize, to : view.window)
+            contentTxtView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardEndframe.height, right: 0)
+            contentTxtView.scrollIndicatorInsets = contentTxtView.contentInset
+
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        adjustKeyboardDismissGesture(isKeyboardVisible: false)
+            contentTxtView.contentInset = UIEdgeInsets.zero
+            self.view.layoutIfNeeded()
+    }
+    
+    func adjustKeyboardDismissGesture(isKeyboardVisible: Bool) {
+        if isKeyboardVisible {
+            if keyboardDismissGesture == nil {
+                keyboardDismissGesture = UITapGestureRecognizer(target: self, action: #selector(tapBackground))
+                view.addGestureRecognizer(keyboardDismissGesture!)
+            }
+        } else {
+            if keyboardDismissGesture != nil {
+                view.removeGestureRecognizer(keyboardDismissGesture!)
+                keyboardDismissGesture = nil
+            }
+        }
+    }
+    
+    @objc func tapBackground() {
+        self.view.endEditing(true)
+    }
+}
+
