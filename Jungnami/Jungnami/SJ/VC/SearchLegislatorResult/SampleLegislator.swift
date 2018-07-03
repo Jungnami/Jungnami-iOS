69a2c13a38a840a0d8e4b7b924749cc4f66b3423
강수진 <rkdthd1234@naver.com>
2018-07-04 00:00:44 +0900

    SearchLegislatorResult View 구성

---
+++ Jungnami/Jungnami/SJ/VC/SearchLegislatorResult/SampleLegislator.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLegislator.swift
+//  Jungnami
+//
+//  Created by 강수진 on 2018. 7. 3..
+//  Copyright © 2018년 강수진. All rights reserved.
+//
+
+import UIKit
+
+struct SampleLegislator{
+    var profile : UIImage
+    var name : String
+    var likeCount : Int
+    var dislikeCount : Int
+    var region : String
+}
---
+++ Jungnami/Jungnami/SJ/VC/SearchLegislatorResult/SearchLegislatorResultTVCell.swift
@@ -0,0 +1,38 @@
+//
+//  SearchLegislatorResultTVCell.swift
+//  Jungnami
+//
+//  Created by 강수진 on 2018. 7. 3..
+//  Copyright © 2018년 강수진. All rights reserved.
+//
+
+import UIKit
+
+class SearchLegislatorResultTVCell: UITableViewCell {
+
+    @IBOutlet weak var rankLbl: UILabel!
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var rankDetailLbl: UILabel!
+    @IBOutlet weak var regionLbl: UILabel!
+    
+    func configure(rank : Int, data : SampleLegislator){
+        rankLbl.text = "\(rank)"
+        profileImg.image = data.profile
+        nameLbl.text = data.name
+        rankDetailLbl.text = "호감 \(data.likeCount)위 / 비호감 \(data.dislikeCount)위"
+        regionLbl.text = data.region
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        profileImg.makeImageRound()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
