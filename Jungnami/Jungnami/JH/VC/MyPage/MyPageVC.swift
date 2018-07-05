5264b1ea293e9d197ebab3c76b722b8090f8b755
add abc <>
2018-07-05 18:47:18 +0900

    1. scrap 완성

--- Jungnami/Jungnami/JH/VC/MyPage/MyPageVC.swift
+++ Jungnami/Jungnami/JH/VC/MyPage/MyPageVC.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class MyPageVC: UIViewController {
 
+    @IBOutlet weak var profileImgView: UIImageView!
+    @IBOutlet weak var profileuserNameLbl: UILabel!
+    
+    @IBOutlet weak var profileScrapNumLbl: UILabel!
+    @IBOutlet weak var profileMyfeedNumLbl: UILabel!
+    @IBOutlet weak var profileFollowingNumLbl: UILabel!
+    @IBOutlet weak var profileFollowerNumLbl: UILabel!
+    
+    //알림, 설정, 버튼으로 연결해야함
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
