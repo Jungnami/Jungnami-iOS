bd7d2d8b520cab281ba2688fd55fd274d8b6ae1c
강수진 <rkdthd1234@naver.com>
2018-07-06 06:09:28 +0900

    메인 뷰 네비게이션 바 커스텀

--- Jungnami/Jungnami/SJ/VC/Main/MainPageMenuVC.swift
+++ Jungnami/Jungnami/SJ/VC/Main/MainPageMenuVC.swift
@@ -8,8 +8,44 @@
 import UIKit
 
 class MainPageMenuVC: UIViewController {
-
+    
     @IBOutlet weak var containerView: UIView!
+    var keyboardDismissGesture: UITapGestureRecognizer?
+    lazy var navSearchView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    lazy var searchGrayView : UIImageView = {
+        let imgView = UIImageView()
+        
+        imgView.image = #imageLiteral(resourceName: "community_search_field")
+        return imgView
+    }()
+    
+    lazy var searchView : UIImageView = {
+        let imgView = UIImageView()
+        
+        imgView.image = #imageLiteral(resourceName: "community_search")
+        return imgView
+    }()
+    
+    lazy var searchTxtField : UITextField = {
+        let txtField = UITextField()
+        txtField.placeholder = "찾고 싶은 국회의원을 검색해보세요"
+        txtField.font = UIFont.systemFont(ofSize: 14.0)
+        return txtField
+    }()
+    
+    lazy var blackView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.4
+        return view
+    }()
+    
+    
     
     private lazy var mainLikeTVC: MainLikeTVC = {
         
@@ -17,7 +53,7 @@ class MainPageMenuVC: UIViewController {
         
         var viewController = storyboard.instantiateViewController(withIdentifier: MainLikeTVC.reuseIdentifier) as! MainLikeTVC
         
-    
+        
         self.add(asChildViewController: viewController)
         
         return viewController
@@ -28,7 +64,7 @@ class MainPageMenuVC: UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         
         var viewController = storyboard.instantiateViewController(withIdentifier: MainDislikeTVC.reuseIdentifier) as! MainDislikeTVC
-      
+        
         self.add(asChildViewController: viewController)
         
         return viewController
@@ -40,20 +76,48 @@ class MainPageMenuVC: UIViewController {
         mb.homeController = self
         return mb
     }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+        setKeyboardSetting()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTxtField.delegate = self
+        setDefaultNav()
+        blackView.isHidden = true
+        self.view.addSubview(blackView)
+        blackView.snp.makeConstraints { (make) in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        }
         setupMenuBar()
         setupView()
         self.navigationController?.navigationBar.shadowImage = UIImage()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
     func scrollToMenuIndex(menuIndex: Int) {
         updateView(selected: menuIndex)
         
     }
-
-   
-
+    
+    func searchLegislator(){
+        if let searchLegislatorResultTVC = self.storyboard?.instantiateViewController(withIdentifier:SearchLegislatorResultTVC.reuseIdentifier) as? SearchLegislatorResultTVC {
+            self.navSearchView.endEditing(true)
+            //searchLegislatorResultTVC = self.selectedCategory
+            self.navigationController?.pushViewController(searchLegislatorResultTVC, animated: true)
+        }
+    }
+    
+    
+    
 }
 //메뉴바랑 그 안 컨테이너뷰
 extension MainPageMenuVC{
@@ -127,4 +191,164 @@ extension MainPageMenuVC{
         
         
     }
+}
+
+//네비게이션 기본바 커스텀
+extension MainPageMenuVC {
+    
+    @objc func setDefaultNav(){
+        //setupTitleNavImg
+        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "partylist_logo"))
+        titleImageView.contentMode = .scaleAspectFit
+        titleImageView.snp.makeConstraints { (make) in
+            make.height.equalTo(21)
+            make.width.equalTo(52)
+        }
+        navigationItem.titleView = titleImageView
+        navigationController?.navigationBar.isTranslucent = false
+        
+        //setupLeftNavItem
+        let myPageBtn = UIButton(type: .system)
+        myPageBtn.setImage(#imageLiteral(resourceName: "partylist_mypage").withRenderingMode(.alwaysOriginal), for: .normal)
+        myPageBtn.addTarget(self, action:  #selector(MainPageMenuVC.toMyPage(_sender:)), for: .touchUpInside)
+        myPageBtn.snp.makeConstraints { (make) in
+            make.height.equalTo(24)
+            make.width.equalTo(24)
+        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: myPageBtn)
+        
+        //setupRightNavItem
+        let searchBtn = UIButton(type: .system)
+        searchBtn.setImage(#imageLiteral(resourceName: "partylist_search").withRenderingMode(.alwaysOriginal), for: .normal)
+        searchBtn.snp.makeConstraints { (make) in
+            make.height.equalTo(24)
+            make.width.equalTo(24)
+        }
+        searchBtn.addTarget(self, action:  #selector(MainPageMenuVC.search(_sender:)), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: searchBtn)
+        
+    }
+    
+    
+    
+}
+
+//기본 네비게이션 바에서 오른쪽/왼쪽 아이템에 대한 행동
+extension MainPageMenuVC {
+    @objc public func toMyPage(_sender: UIButton) {
+        //1. 나중에 goFirst 했던 것처럼 해당 뷰로 exit 바로 할수 있도록 하기
+    }
+    
+    @objc public func search(_sender: UIButton) {
+        makeSearchBarView()
+        self.navigationItem.leftBarButtonItem = nil
+        
+    }
+    
+    
+}
+
+//네비게이션 서치바 커스텀
+extension MainPageMenuVC{
+    func makeSearchBarView() {
+        navSearchView.snp.makeConstraints { (make) in
+            make.width.equalTo(311)
+            make.height.equalTo(31)
+            // make.leading.equalTo(self.)
+        }
+        navSearchView.addSubview(searchGrayView)
+        navSearchView.addSubview(searchView)
+        navSearchView.addSubview(searchTxtField)
+        
+        searchGrayView.snp.makeConstraints { (make) in
+            make.top.bottom.leading.trailing.equalTo(navSearchView)
+        }
+        
+        searchView.snp.makeConstraints { (make) in
+            make.leading.equalTo(searchGrayView).offset(10)
+            make.width.height.equalTo(15)
+            make.centerY.equalTo(searchGrayView)
+        }
+        
+        searchTxtField.snp.makeConstraints { (make) in
+            make.top.bottom.trailing.equalTo(searchGrayView)
+            make.leading.equalTo(searchView.snp.trailing).offset(8)
+        }
+        
+        searchTxtField.delegate = self
+        navigationItem.titleView = navSearchView
+        navigationController?.navigationBar.isTranslucent = false
+        
+        //rightBarBtn
+        let rightBarButton = customBarbuttonItem(title: "취소", red: 112, green: 112, blue: 112, fontSize: 14, selector: #selector(setDefaultNav))
+        
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
+}
+
+//txtField Delegate (엔터버튼 클릭시)
+extension MainPageMenuVC : UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            simpleAlert(title: "오류", message: "검색어 입력")
+            return false
+        }
+        
+        if let myString = textField.text {
+            let emptySpacesCount = myString.components(separatedBy: " ").count-1
+            
+            if emptySpacesCount == myString.count {
+                simpleAlert(title: "오류", message: "검색어 입력")
+                return false
+            }
+        }
+        searchLegislator()
+        return true
+    }
+}
+
+//키보드 대응
+extension MainPageMenuVC{
+    func setKeyboardSetting() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        blackView.isHidden = false
+        adjustKeyboardDismissGesture(isKeyboardVisible: true)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        blackView.isHidden = true
+        searchTxtField.text = ""
+        // setDefaultNav()
+        adjustKeyboardDismissGesture(isKeyboardVisible: false)
+        
+    }
+    
+    //화면 바깥 터치했을때 키보드 없어지는 코드
+    func adjustKeyboardDismissGesture(isKeyboardVisible: Bool) {
+        if isKeyboardVisible {
+            if keyboardDismissGesture == nil {
+                keyboardDismissGesture = UITapGestureRecognizer(target: self, action: #selector(tapBackground))
+                view.addGestureRecognizer(keyboardDismissGesture!)
+            }
+        } else {
+            if keyboardDismissGesture != nil {
+                view.removeGestureRecognizer(keyboardDismissGesture!)
+                keyboardDismissGesture = nil
+            }
+        }
+    }
+    
+    @objc func tapBackground() {
+        self.navSearchView.endEditing(true)
+    }
+    
+    
+    
+    
+    
+    
 }
