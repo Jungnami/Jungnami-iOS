d3b37adc1c34880ce653c7843cabf543088f0288
add abc <>
2018-07-08 20:50:06 +0900

    1. 팔로우,팔로워 샘플데이터 생성 -> 아직 완벽히 데이터 뿌려보지못함 2. 팔로워,팔로잉 뷰 완성

---
+++ Jungnami/Jungnami/JH/VC/MyPage/FollowFollowing/Follow/FollowListVC.swift
@@ -0,0 +1,49 @@
+//
+//  FollowListVC.swift
+//  Jungnami
+//
+//  Created by 이지현 on 2018. 7. 8..
+//
+
+import UIKit
+
+class FollowListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var followTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        followTableView.delegate = self
+        followTableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+ static var data = FollowListData.sharedInstance.followers
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+                return 1
+        }else {
+            //data로 연결
+            return 3
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0{
+        let cell = tableView.dequeueReusableCell(withIdentifier: FollowFixedCell.reuseIdentifier, for: indexPath) as! FollowFixedCell
+            cell.followFixedLbl.text = "사람"
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: FollowCell.reuseIdentifier, for: indexPath) as! FollowCell
+            cell.configure(data: FollowListVC.data[indexPath.row])
+            return cell
+        }
+        
+    }
+}
+
