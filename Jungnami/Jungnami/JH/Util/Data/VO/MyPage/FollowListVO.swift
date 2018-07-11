447476cdac0315c7d6ec44560d858ee022bb946e
add abc <>
2018-07-12 05:04:09 +0900

    팔로잉 팔로워 VO, Service 만들엇

---
+++ Jungnami/Jungnami/JH/Util/Data/VO/MyPage/FollowListVO.swift
@@ -0,0 +1,24 @@
+//
+//  FollowListVO.swift
+//  Jungnami
+//
+//  Created by 이지현 on 2018. 7. 12..
+//
+
+import Foundation
+
+struct FollowListVO: Codable {
+    let message: String
+    let data: [FollowListVOData]
+}
+
+struct FollowListVOData: Codable {
+    let followingID, followingNickname, followingImgURL, isMyFollowing: String
+    
+    enum CodingKeys: String, CodingKey {
+        case followingID = "following_id"
+        case followingNickname = "following_nickname"
+        case followingImgURL = "following_img_url"
+        case isMyFollowing
+    }
+}
