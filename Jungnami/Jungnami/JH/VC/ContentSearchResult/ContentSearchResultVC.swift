1808c7c8edfd8693d6cd71906e3fe30c12bd2e44
add abc <>
2018-07-13 10:23:01 +0900

    contentSearchResult View finish

--- Jungnami/Jungnami/JH/VC/ContentSearchResult/ContentSearchResultVC.swift
+++ Jungnami/Jungnami/JH/VC/ContentSearchResult/ContentSearchResultVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ContentSearchResultVC: UIViewController {
+class ContentSearchResultVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     
 
@@ -19,24 +19,25 @@ class ContentSearchResultVC: UIViewController {
         super.viewDidLoad()
         contentSearchResultCollectionView.delegate = self
         contentSearchResultCollectionView.dataSource = self
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-}
-
-extension ContentSearchResultVC:  UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return sampleData.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = contentSearchResultCollectionView.dequeueReusableCell(withReuseIdentifier: ContentSearchResultCell.reuseIdentifier, for: [indexPath.row]) as! ContentSearchResultCell
+        let cell = contentSearchResultCollectionView.dequeueReusableCell(withReuseIdentifier: ContentSearchResultCell.reuseIdentifier, for: indexPath) as! ContentSearchResultCell
         cell.configure(data: sampleData[indexPath.row])
+        
+        cell.contentImgView.layer.cornerRadius = 10
+        cell.contentImgView.layer.masksToBounds = true
+        
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -51,4 +52,5 @@ extension ContentSearchResultVC:  UICollectionViewDelegate, UICollectionViewData
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
+    
 }
